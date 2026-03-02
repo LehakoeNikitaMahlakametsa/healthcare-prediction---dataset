d479d90d9613dfa686435c57b9e3a87ac57a5748
Lehakoe Nikita Mahlakametsa <nikitamahlakametsa@gmail.com>
2026-03-02 12:22:13 +0200

    Update healthcare-analysis.sql

--- healthcare-analysis.sql
+++ healthcare-analysis.sql
@@ -3,6 +3,40 @@ FROM healthcare_prediction_staging
 GROUP BY age
 ORDER BY patient_count DESC;
 
+SELECT gender, COUNT(*) AS patient_count
+FROM healthcare_prediction_staging
+GROUP BY gender
+ORDER BY patient_count DESC;
+
+SELECT gender,
+       COUNT(*) AS patient_count,
+       ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM healthcare_prediction_staging), 2) AS percentage
+FROM healthcare_prediction_staging
+GROUP BY gender
+ORDER BY patient_count DESC;
+
+SELECT gender, admission_type, COUNT(*) AS count
+FROM healthcare_prediction_staging
+GROUP BY gender, admission_type
+ORDER BY gender, count DESC;
+
+SELECT blood_type, COUNT(*) AS patient_count
+FROM healthcare_prediction_staging
+GROUP BY blood_type
+ORDER BY patient_count DESC;
+
+SELECT blood_type,
+       COUNT(*) AS patient_count,
+       ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM healthcare_prediction_staging), 2) AS percentage
+FROM healthcare_prediction_staging
+GROUP BY blood_type
+ORDER BY patient_count DESC;
+
+SELECT medical_condition, COUNT(*)
+FROM healthcare_prediction_staging
+GROUP BY medical_condition
+ORDER BY COUNT(*) DESC;
+
 SELECT medical_condition,
 CASE
 WHEN age < 18 THEN 'Child'
@@ -43,3 +77,15 @@ AVG(DATEDIFF(discharge_date, date_of_admission)) AS avg_stay,
 FROM healthcare_prediction_staging
 GROUP BY age_group
 ORDER BY avg_billing DESC;
+
+SELECT doctor, COUNT(*)
+FROM healthcare_prediction_staging
+GROUP BY doctor
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC;
+
+SELECT hospital, COUNT(*) AS admission_count
+FROM healthcare_prediction_staging
+GROUP BY hospital
+HAVING COUNT(*) > 1
+ORDER BY admission_count DESC;
